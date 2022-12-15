a8749bdabee173bf03bbcccda0767b4eab72c800
Cyriel89 <anthony.carrer@hotmail.fr>
2022-12-15 11:35:26 +0100

    Add Data publication to mqtt + node red

--- src/main.cpp
+++ src/main.cpp
@@ -7,13 +7,21 @@
 #include <ArduinoJson.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_MPU6050.h>
+#include <PubSubClient.h>
 
 const int DHTPIN = 14; // what digital pin we're connected to
 const int DHTTYPE = DHT22; // DHT 22 (AM2302), AM2321
 const int LEDPIN = 19; // what digital pin we're connected to
 const char* ssid = "iPhone de Lucas";
 const char* password = "babayaga";
-
+const char* mqtt_server = "172.20.10.2";
+//Create WiFiClient object
+WiFiClient espClient;
+// Create PubSubClient object
+PubSubClient client(espClient);
+long lastMsg = 0;
+char msg[50];
+int value = 0;
 // Timer variables
 unsigned long lastTime = 0;  
 unsigned long lastTimeTemp = 0;
@@ -21,31 +29,23 @@ unsigned long lastTimeAcc = 0;
 unsigned long gyroDelay = 10;
 unsigned long tempDelay = 2000;
 unsigned long accDelay = 200;
-
 // Create AsyncWebServer object on port 80
 AsyncWebServer server(80);
-
 // Create an Event Source on /events
 AsyncEventSource events("/events");
-
 // Json variable to hold Sensor Readings
 StaticJsonDocument<200> docReadings;
-
 // Variable to store the HTTP request
 String header;
-
 // MPU6050 sensor
 Adafruit_MPU6050 mpu;
-
+// Event variables
 sensors_event_t a, g, temp;
-
 // MPU6050 sensor variables
 float ax, ay, az;
 float gx, gy, gz;
-
 // DHT sensor
 DHT dht(DHTPIN, DHTTYPE);
-
 // DHT sensor variables
 float h;
 float t;
@@ -104,6 +104,44 @@ void initWiFi() {
   server.begin();
 }
 
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived on topic: ");
+  Serial.print(topic);
+  Serial.print(". Message: ");
+  String messageTemp;
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+    messageTemp += (char)payload[i];
+  }
+  Serial.println();
+}
+
+// Init MQTT
+void initMQTT() {
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+}
+
+// Reconnect to MQTT
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.println("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("ESP32Client")) {
+      Serial.println("connected");
+      // Subscribe
+      client.subscribe("esp32/output");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
 String getGyroReadings() {
   mpu.getEvent(&a, &g, &temp);
   gx = g.gyro.x;
@@ -148,6 +186,7 @@ void setup() {
   initLED();
   initSPIFFS();
   initWiFi();
+  initMQTT();
 
   // Send a GET request to <ESP_IP>/get?message=<message>
   server.on("/get", HTTP_GET, [](AsyncWebServerRequest *request){
@@ -172,11 +211,36 @@ void setup() {
 }
 
 void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  long currentMillis = millis();
+  if (currentMillis - lastMsg > 5000) {
+    lastMsg = currentMillis;
+    // Publish Temperature
+    client.publish("esp32/temperature", docReadings["temp"].as<String>().c_str());
+    // Publish Humidity
+    client.publish("esp32/humidity", docReadings["hum"].as<String>().c_str());
+    // Publish GyroX
+    client.publish("esp32/gyroX", docReadings["gx"].as<String>().c_str());
+    // Publish GyroY
+    client.publish("esp32/gyroY", docReadings["gy"].as<String>().c_str());
+    // Publish GyroZ
+    client.publish("esp32/gyroZ", docReadings["gz"].as<String>().c_str());
+    // Publish AccX
+    client.publish("esp32/accX", docReadings["ax"].as<String>().c_str());
+    // Publish AccY
+    client.publish("esp32/accY", docReadings["ay"].as<String>().c_str());
+    // Publish AccZ
+    client.publish("esp32/accZ", docReadings["az"].as<String>().c_str());
+  }
+
   if((millis() - lastTime) > gyroDelay) {
     lastTime = millis();
     events.send(getGyroReadings().c_str(), "gyro_readings", millis());
     lastTime = millis();
-    Serial.println(getGyroReadings().c_str());
   }
 
   if((millis() - lastTimeAcc) > accDelay) {
